54c2b61eb3c6fe410700986cacf6e99e29ce3e68
Pedro Nieto <pedronieto.2005@gmail.com>
2025-04-29 07:46:39 -0300

    feat: Add project

---
+++ src/main/java/com/pedronieto/qrcode/generator/dto/qrcode/QrCodeGenerateRequest.java
@@ -0,0 +1,4 @@
+package com.pedronieto.qrcode.generator.dto.qrcode;
+
+public record QrCodeGenerateRequest(String text) {
+}
---
+++ src/main/java/com/pedronieto/qrcode/generator/ports/StoragePort.java
@@ -0,0 +1,5 @@
+package com.pedronieto.qrcode.generator.ports;
+
+public interface StoragePort {
+    String uploadFile(byte[] fileData, String fileName, String contentType);
+}
